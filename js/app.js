d0761cf84649454aa2f3d71293cf67a1c5858d7e
neilw22 <neilw22@gmail.com>
2015-04-02 15:27:29 -0400

    Added checkboxes and "enter" key press

--- js/app.js
+++ js/app.js
@@ -3,8 +3,12 @@ $(document).ready(function() {
 	$('.add-items').click(function(event){
 		event.preventDefault();
 		$( ".list-items" ).append( "<li>"+$('.itemstoadd').val()+"</li>")
-
-
+	})
+	
+	$('.itemstoadd').keypress(function(event) {
+		if (event.which==13) {
+			$( ".list-items" ).append( "<li>"+$('.itemstoadd').val()+"</li>")
+		};
 	})
 
 
