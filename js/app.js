82814ff1151defd932f643f336fdfbc10bd6db97
neilw22 <neilw22@gmail.com>
2015-04-02 16:04:06 -0400

    Added Checkbox to list items

--- js/app.js
+++ js/app.js
@@ -1,13 +1,16 @@
 $(document).ready(function() {
 
-	$('.add-items').click(function(event){
+	$('.add-items').click(function(event) {
 		event.preventDefault();
-		$( ".list-items" ).append( "<li>"+$('.itemstoadd').val()+"</li>")
+		$( ".list-items" ).append( "<li>"+$('.itemstoadd').val()+"<input type='checkbox'</i>"+"</li>");
+			$( '.itemstoadd').val('');
+
 	})
-	
+
 	$('.itemstoadd').keypress(function(event) {
 		if (event.which==13) {
-			$( ".list-items" ).append( "<li>"+$('.itemstoadd').val()+"</li>")
+			$( ".list-items" ).append( "<li>"+$('.itemstoadd').val()+"<input type='checkbox'</i>"+"</li>");
+				$( '.itemstoadd').val('');
 		};
 	})
 
