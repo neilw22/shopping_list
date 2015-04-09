65276e2dbea24593307d3f5343fe72b96d9c9a0c
neilw22 <neilw22@gmail.com>
2015-04-09 12:44:35 -0400

    Added alert for empty input

--- js/app.js
+++ js/app.js
@@ -1,34 +1,7 @@
 $(document).ready(function() {
 
-	$(function () {
-    $(".itemstoadd").keypress(function (e) {
-        if (e.which == 13) {
-            var content = $(".itemstoadd").val();
-            var li  = $('<li />', {
-                    text : content
-                }),
-                inp = $('<input />', {
-                    'class': 'check',
-                    type   : 'checkbox',
-                    on : {
-                        change: function() {
-                            if (this.checked) {
-                                $(this).closest('li').wrapInner('<del />');
-                            }else {
-                                $(this).unwrap();
-                            }
-                        }
-                    }
-                });
-            $('.list-items').append( li.append(inp) );
-            $(".itemstoadd").val('');
-            $('#default').hide();
-        };
-    });
-});
+function addItemtoList() {
 
-	$(function () {
-    $(".add-items").click(function (e) {
             var content = $(".itemstoadd").val();
             var li  = $('<li />', {
                     text : content
@@ -46,27 +19,37 @@ $(document).ready(function() {
                         }
                     }
                 });
-                // console.log(content);
+
+            console.log(content);
+            if (content == ('')) {
+                alert("Please enter an item");
+            }
+
+            else {
             $('.list-items').append( li.append(inp) );
             $(".itemstoadd").val('');
             $('#default').hide();
+            };
+        };
+        
+$(".itemstoadd").keypress(function (e) {
+        if (e.which == 13) {
+            addItemtoList();
+        }
     });
-});
+
+$(".add-items").click(function (e) {
+    
+            addItemtoList();
+            
+        });
 
 $('.reset').click(function(event) {
 	$('.list-items').empty();
     $('#default').fadeIn( "slow" );
-});
-
-// $('.email').click(function(event) {
-// 	alert("Coming Soon!")
-// });
+    });
 
 $('.email').click(function() {
-        
-        
-        
-            // var name = $('#name').val();
             var email = $('#email').val();
             var list = $('.list-items').text();
             var varData = 'email=' + email + '&list=' + list;
@@ -82,28 +65,6 @@ $('.email').click(function() {
             
             });
         
-        });
-
-
-// $('.add-items').click(function(additem));
-
-	// $('.add-items').click(function(event) {
-	// 	event.preventDefault();
-	// 	$( ".list-items" ).append( "<li>"+$('.itemstoadd').val()+"<input class='checkbox' type='checkbox'</i>"+"</li>");
-	// 		$( '.itemstoadd').val('');
-
-	// });
-
-	// $('.itemstoadd').keypress(function(event) {
-	// 	if (event.which==13) {
-	// 		$( ".list-items" ).append( "<li>"+$('.itemstoadd').val()+"<input type='checkbox'</i>"+"</li>");
-	// 			$( '.itemstoadd').val('');
-	// 	};
-	// })
-
-	// $('.checkbox').click(function () {
-	// 	$(this).find('li').addClass("crossed-off");
-	// 		// $(this).addClass("crossed-off");
-	// });
+         });
 
 });
